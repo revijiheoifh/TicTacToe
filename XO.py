a87d1299ad445940e50c07761b2b3200e7ba8893
revijiheoifh <52609155+revijiheoifh@users.noreply.github.com>
2023-05-24 00:29:20 +0100

    Update XO.py

--- XO.py
+++ XO.py
@@ -2,109 +2,11 @@ row1 = ["","",""]
 row2 = ["","",""]
 row3 = ["","",""]
 
-def X(inp):
-    if inp == "TL" and row1[0] == "":
-        row1[0] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "TM" and row1[1] == "":
-        row1[1] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "TR" and row1[2] == "":
-        row1[2] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-
-    elif inp == "ML" and row2[0] == "":
-        row2[0] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "MM" and row2[1] == "":
-        row2[1] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "MR" and row2[2] == "":
-        row2[2] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-
-    elif inp == "BL" and row3[0] == "":
-        row3[0] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "BM" and row3[1] == "":
-        row3[1] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "BR" and row3[2] == "":
-        row3[2] = 'x'
-        print(row1)
-        print(row2)
-        print(row3)
-
-def O(inp):
-    if inp == "TL" and row1[0] == "":
-        row1[0] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "TM" and row1[1] == "":
-        row1[1] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "TR" and row1[2] == "":
-        row1[2] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-
-    elif inp == "ML" and row2[0] == "":
-        row2[0] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "MM" and row2[1] == "":
-        row2[1] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "MR" and row2[2] == "":
-        row2[2] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-
-    elif inp == "BL" and row3[0] == "":
-        row3[0] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "BM" and row3[1] == "":
-        row3[1] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-    elif inp == "BR" and row3[2] == "":
-        row3[2] = 'o'
-        print(row1)
-        print(row2)
-        print(row3)
-
 def check_winner():
     # Check rows
     for row in [row1, row2, row3]:
         if row[0] == row[1] == row[2] != "":
-            return row[0]      
+            return row[0]
 
     # Check columns
     for col in range(3):
@@ -124,25 +26,3 @@ def check_draw():
         if "" in row:
             return False
     return True
-
-while True:
-    inp = input("where do you want to play? (X) ")
-    X(inp)
-    winner = check_winner()
-    if winner:
-        print(f"Player {winner} wins!")
-        break
-    elif check_draw():
-        print("Draw!")
-        break
-
-    inp = input("where do you want to play? (O) ")
-    O(inp)
-    winner = check_winner()
-
-    if winner:
-        print(f"Player {winner} wins!")
-        break
-    elif check_draw():
-        print("Draw!") 
-        break
